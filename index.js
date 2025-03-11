515056a1482e3d711bf1409e4da83328fce994cd
Andrey <Andreyvol9898@gmail.com>
2025-03-11 17:50:55 +0500

    first

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 addListeners();
 
+function animaster() {
+    return {
+        fadeIn: function(element, duration) {
+            element.style.transitionDuration =  `${duration}ms`;
+            element.classList.remove('hide');
+            element.classList.add('show');
+        },
+
+        move: function(element, duration, translation) {
+            element.style.transitionDuration = `${duration}ms`;
+            element.style.transform = this.getTransform(translation, null);
+        },
+
+        scale: function(element, duration, ratio) {
+            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transform = this.getTransform(null, ratio);
+        },
+
+        getTransform: function(translation, ratio) {
+            const result = [];
+            if (translation) {
+                result.push(`translate(${translation.x}px,${translation.y}px)`);
+            }
+            if (ratio) {
+                result.push(`scale(${ratio})`);
+            }
+            return result.join(' ');
+        }
+    }
+}
+
 function addListeners() {
+    let animasterInstancer = animaster()
+
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animasterInstance.fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animasterInstancer.move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animasterInstancer.scale(block, 1000, 1.25);
         });
 }
 
@@ -25,11 +58,11 @@ function addListeners() {
  * @param element — HTMLElement, который надо анимировать
  * @param duration — Продолжительность анимации в миллисекундах
  */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
+// function fadeIn(element, duration) {
+//     element.style.transitionDuration =  `${duration}ms`;
+//     element.classList.remove('hide');
+//     element.classList.add('show');
+// }
 
 /**
  * Функция, передвигающая элемент
@@ -37,10 +70,10 @@ function fadeIn(element, duration) {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param translation — объект с полями x и y, обозначающими смещение блока
  */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
+// function move(element, duration, translation) {
+//     element.style.transitionDuration = `${duration}ms`;
+//     element.style.transform = getTransform(translation, null);
+// }
 
 /**
  * Функция, увеличивающая/уменьшающая элемент
@@ -48,18 +81,19 @@ function move(element, duration, translation) {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
  */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
+// function scale(element, duration, ratio) {
+//     element.style.transitionDuration =  `${duration}ms`;
+//     element.style.transform = getTransform(null, ratio);
+// }
 
-function getTransform(translation, ratio) {
-    const result = [];
-    if (translation) {
-        result.push(`translate(${translation.x}px,${translation.y}px)`);
-    }
-    if (ratio) {
-        result.push(`scale(${ratio})`);
-    }
-    return result.join(' ');
-}
+// function getTransform(translation, ratio) {
+//     const result = [];
+//     if (translation) {
+//         result.push(`translate(${translation.x}px,${translation.y}px)`);
+//     }
+//     if (ratio) {
+//         result.push(`scale(${ratio})`);
+//     }
+//     return result.join(' ');
+// }
+
